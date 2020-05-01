9c45bad387d76858780a0837d7adb420c7ffcafa
WFs882 <64633973+WFs882@users.noreply.github.com>
2020-05-01 14:10:11 +0200

    Add files via upload

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
 from torchvision.datasets import VisionDataset
 
 from PIL import Image
@@ -20,25 +14,29 @@ def pil_loader(path):
         return img.convert('RGB')
 
 
-def make_dataset(directory, class_to_idx):
-    filePath = "Caltech101/" + self.split + ".txt"
+def make_dataset(directory, class_to_idx, split):
+    instances = []
+    dataset = []
+    #directory: Caltech101/101_ObjectCategories
+    root, tail = os.path.split(directory)
+    filePath = root + "/" + split + ".txt"
     file = open(filePath, "r")
     number_of_lines = len(file.readlines())
     file = open(filePath, "r")
     
-    instances = []
-    dataset = []
+    
     directory = os.path.expanduser(directory)
     for target_class in sorted(class_to_idx.keys(),key=str.lower):
         #print(target_class)
         class_index = class_to_idx[target_class]
-        target_dir = os.path.join(root, target_class)
+        target_dir = os.path.join(directory, target_class)
         if not os.path.isdir(target_dir):
             continue
         for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
             for fname in sorted(fnames):
                 head, tail = os.path.split(root)
                 path = os.path.join(tail, fname)
+                item = path, class_index
                 instances.append(item)
 
     num_of_instances=len(instances)
@@ -75,49 +73,8 @@ class Caltech(VisionDataset):
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
-        classes, classes_to_idx = self._find_classes(self.root)
-        '''
-        instances = []
-        dataset = []
-
-        filePath = "Caltech101/" + split + ".txt"
-        file = open(filePath, "r")
-        number_of_lines = len(file.readlines())
-        #print(number_of_lines)
-        file = open(filePath, "r")
-
-        for target_class in sorted(class_to_idx.keys(),key=str.lower):
-            #print(target_class)
-            class_index = class_to_idx[target_class]
-            target_dir = os.path.join(root, target_class)
-            if not os.path.isdir(target_dir):
-                continue
-            for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
-                for fname in sorted(fnames):
-                    head, tail = os.path.split(root)
-                    path = os.path.join(tail, fname)
-                    item = path, class_index
-                    instances.append(item)
-
-        num_of_instances=len(instances)
-
-        i=0
-        next=True
-        while(i<num_of_instances):
-            if(next is True):
-                next = False
-                currentImage = file.readline()
-                currentImage = currentImage.replace("/", "\\").replace("\n","")
-            if(currentImage.find("Google")!=-1):
-                next = True
-            elif(currentImage == instances[i][0]):
-                dataset.append(instances[i])
-                next = True
-                i=i+1
-            else:
-                i=i+1
-        '''
-        samples = make_dataset(self.root, class_to_idx)
+        classes, class_to_idx = self._find_classes(self.root)
+        samples = make_dataset(self.root, class_to_idx, self.split)
         self.classes = classes
         self.class_to_idx = class_to_idx
         self.samples = samples
@@ -136,7 +93,7 @@ class Caltech(VisionDataset):
         Ensures:
             No class is a subdirectory of another.
         """
-        classes = [d.name for d in os.scandir(root) if (d.is_dir() and d.name!="BACKGROUND_Google")]
+        classes = [d.name for d in os.scandir(dir) if (d.is_dir() and d.name!="BACKGROUND_Google")]
         classes.sort()
         class_to_idx = {classes[i]: i for i in range(len(classes))}
         return classes, class_to_idx    
@@ -173,83 +130,3 @@ class Caltech(VisionDataset):
         It is mandatory, as this is used by several other components
         '''
         return len(self.samples) # Provide a way to get the length (number of elements) of the dataset
-
-
-# In[11]:
-
-
-import os
-import logging
-
-import random
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Subset, DataLoader
-from torch.backends import cudnn
-
-import torchvision
-from torchvision import transforms
-from torchvision.models import alexnet
-
-from PIL import Image
-from tqdm import tqdm
-
-
-# In[12]:
-
-
-DEVICE = 'cuda' # 'cuda' or 'cpu'
-
-NUM_CLASSES = 102 # 101 + 1: There is an extra Background class that should be removed 
-
-BATCH_SIZE = 256     # Higher batch sizes allows for larger learning rates. An empirical heuristic suggests that, when changing
-                     # the batch size, learning rate should change by the same factor to have comparable results
-
-LR = 1e-3            # The initial Learning Rate
-MOMENTUM = 0.9       # Hyperparameter for SGD (Stochastic Gradient Descent), keep this at 0.9 when using SGD
-WEIGHT_DECAY = 5e-5  # Regularization, you can keep this at the default
-
-NUM_EPOCHS = 30      # Total number of training epochs (iterations over dataset)
-STEP_SIZE = 20       # How many epochs before decreasing learning rate (if using a step-down policy)
-GAMMA = 0.1          # Multiplicative factor for learning rate step-down
-
-LOG_FREQUENCY = 10
-
-
-# In[13]:
-
-
-# Define transforms for training phase
-train_transform = transforms.Compose([transforms.Resize(256),      # Resizes short size of the PIL image to 256
-                                      transforms.CenterCrop(224),  # Crops a central square patch of the image
-                                                                   # 224 because torchvision's AlexNet needs a 224x224 input!
-                                                                   # Remember this when applying different trans…                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # Normalizes tensor with mean and standard deviation
-])
-# Define transforms for the evaluation phase
-eval_transform = transforms.Compose([transforms.Resize(256),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))                                    
-])
-
-
-# In[14]:
-
-
-DATA_DIR = 'Caltech101/101_ObjectCategories'
-from Caltech101.caltech_dataset import Caltech
-
-# Prepare Pytorch train/test Datasets
-train_dataset = Caltech(DATA_DIR, split='train',  transform=train_transform)
-test_dataset = Caltech(DATA_DIR, split='test', transform=eval_transform)
-
-print(train_dataset)
-
-
-# In[ ]:
-
-
-
-
